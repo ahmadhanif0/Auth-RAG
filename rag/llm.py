4e4c52349d3846a2b40837fed12716e0ad34e773
ahmadhanif0 <ahmadhanif7895@gmail.com>
2026-05-08 03:28:13 +0500

    Minor changes

--- rag/llm.py
+++ rag/llm.py
@@ -1,9 +1,15 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
+api_key = os.getenv("GROQ_API_KEY")
+
+print("API KEY LOADED:", api_key is not None)  # debug
+
 llm = ChatGroq(
+    api_key=api_key,
     model="llama-3.3-70b-versatile",
     temperature=0
 )
